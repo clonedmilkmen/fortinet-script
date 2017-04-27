b4d449ee72dc83bdf4d715895bf239878190b3aa
clonedmilkmen <michael@winterstorm.ca>
2017-04-27 17:17:03 -0600

    Added fmg-json2csv.pl
    Added a script to parse data.json files exported from FortiManager 5.4

---
+++ fmg-json2csv.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+use JSON;
+
+##
+## usage: fmg-json2csv.pl > output.csv
+
+$json = JSON->new->allow_nonref;
+
+local $/ = undef;
+open FILE, 'data.json' or die "Could not open data.json";
+binmode FILE;
+$data = <FILE>;
+close FILE;
+
+$fmg = $json->decode($data);
+
+@cols = ('name', 'sn', 'ip', 'platform_str');
+
+foreach $device (@{$fmg->{'params'}->[0]->{'data'}}) {
+  @vals = ();
+  foreach $col (@cols) {
+    push @vals, $device->{$col};
+  }
+  printf("%s\n",
+    join ',', @vals
+    );
+}
